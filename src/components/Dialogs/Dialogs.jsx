ddca3af4a178c6376b13a6e03aef44a3eece037e
blackpanther <fakhrudinov.azamat@gmail.com>
2021-06-19 18:20:53 +0600

    video-21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,30 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Abay
+                </div>
+                <div className={s.dialog}>
+                    Dulat
+                </div>
+                <div className={s.dialog}>
+                    Nurbolat
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    Hello
+                </div>
+                <div className={s.message}>
+                    mmm
+                </div>
+            </div>
         </div>
     );
 }
