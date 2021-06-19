ab939cf028f2ff30d07bf11d5cebccc210826dd6
blackpanther <fakhrudinov.azamat@gmail.com>
2021-06-19 17:46:30 +0600

    video-19

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BrowserRouter, Route} from "react-router-dom"
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
@@ -7,13 +8,16 @@ import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = (props) => {
   return (
-    <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-            <Dialogs />
-        </div>
-    </div>
+      <BrowserRouter>
+          <div className='app-wrapper'>
+              <Header />
+              <Navbar />
+              <div className="app-wrapper-content">
+                  <Route path="/profile" component={Profile} />
+                  <Route path="/dialogs" component={Dialogs} />
+              </div>
+          </div>
+      </BrowserRouter>
   );
 }
 
