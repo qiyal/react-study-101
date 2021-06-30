9c885a6ac9a717340e30caf2be61483ff2e946c2
Yerbolat <twinspazyl@gmail.com>
2021-06-30 09:00:04 +0600

    do-1 video-26

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,53 +1,20 @@
 import React from 'react'
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
-
-const DialogItem = (props) => {
-    return (
-        <div className={`${s.dialog} ${s.active}`}>
-            <NavLink to={`/dialogs/${props.id}`} activeClassName={s.active}>{props.name}</NavLink>
-        </div>
-    );
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.message}>
-            {props.message}
-        </div>
-    );
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
     let dialogs = [
-        {
-            id: 1,
-            name: 'Abay'
-        },
-        {
-            id: 2,
-            name: 'Dulat'
-        },
-        {
-            id: 3,
-            name: 'Nurbolat'
-        }
+        { id: 1, name: 'Abay' },
+        { id: 2, name: 'Dulat' },
+        { id: 3, name: 'Nurbolat' }
     ]
 
     let messages = [
-        {
-            id: 1,
-            message: 'Hi'
-        },
-        {
-            id: 2,
-            message: 'Hello'
-        },
-        {
-            id: 3,
-            message: 'mmm'
-        }
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'Hello'},
+        { id: 3, message: 'mmm'}
     ]
 
     let dialogsElements = dialogs.map(d => <DialogItem id={d.id} name={d.name} />)
