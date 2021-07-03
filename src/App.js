7a431702f529a2e16178d9d74024c9f0f4458c54
blackpanther <fakhrudinov.azamat@gmail.com>
2021-07-03 14:22:00 +0600

    video-28

--- src/App.js
+++ src/App.js
@@ -7,33 +7,14 @@ import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = (props) => {
-    let dialogs = [
-        { id: 1, name: 'Abay' },
-        { id: 2, name: 'Dulat' },
-        { id: 3, name: 'Nurbolat' }
-    ]
-
-    let messages = [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'Hello'},
-        { id: 3, message: 'mmm'}
-    ]
-
-    let posts = [
-        { id: 1, message: 'post 1', likesCount: 0 },
-        { id: 2, message: 'post 2', likesCount: 23 },
-        { id: 3, message: 'post 3', likesCount: 45 },
-        { id: 4, message: 'post 4', likesCount: 89 }
-    ]
-
     return (
       <BrowserRouter>
           <div className='app-wrapper'>
               <Header />
               <Navbar />
               <div className="app-wrapper-content">
-                  <Route path="/profile" render={ () => <Profile posts={posts} /> } />
-                  <Route path="/dialogs" render={ () => <Dialogs dialogs={dialogs} messages={messages} /> } />
+                  <Route path="/profile" render={ () => <Profile posts={props.posts} /> } />
+                  <Route path="/dialogs" render={ () => <Dialogs dialogs={props.dialogs} messages={props.messages} /> } />
               </div>
           </div>
       </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let dialogs = [
+    { id: 1, name: 'Abay' },
+    { id: 2, name: 'Dulat' },
+    { id: 3, name: 'Nurbolat' }
+]
+
+let messages = [
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'Hello'},
+    { id: 3, message: 'mmm'}
+]
+
+let posts = [
+    { id: 1, message: 'post 1', likesCount: 0 },
+    { id: 2, message: 'post 2', likesCount: 23 },
+    { id: 3, message: 'post 3', likesCount: 45 },
+    { id: 4, message: 'post 4', likesCount: 89 }
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
