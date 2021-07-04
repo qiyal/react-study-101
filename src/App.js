6b8fcfa2fe90b3e2d73fa7a1c796d23d83ca562b
Yerbolat <twinspazyl@gmail.com>
2021-07-04 18:28:35 +0600

    video-30

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Route} from "react-router-dom"
+import { Route } from "react-router-dom"
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
@@ -8,16 +8,14 @@ import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = (props) => {
     return (
-      <BrowserRouter>
-          <div className='app-wrapper'>
-              <Header />
-              <Navbar />
-              <div className="app-wrapper-content">
-                  <Route path="/dialogs" render={ () => <Dialogs state={props.state.dialogsPage} /> } />
-                  <Route path="/profile" render={ () => <Profile state={props.state.profilePage} /> } />
-              </div>
-          </div>
-      </BrowserRouter>
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <div className="app-wrapper-content">
+                <Route path="/dialogs" render={ () => <Dialogs state={props.state.dialogsPage} /> } />
+                <Route path="/profile" render={ () => <Profile state={props.state.profilePage} /> } />
+            </div>
+        </div>
     );
 }
 
